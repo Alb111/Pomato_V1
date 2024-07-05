e36cc383e48d6f74de3a8d93858e0c59410b71f9
alb123 <albertfelix2005@gmail.com>
2024-07-05 02:53:16 -0700

    first version of topbar done, need to get auth set up

--- src/lib/components/Navigation/Topbar.svelte
+++ src/lib/components/Navigation/Topbar.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
     let x:boolean = true
-    import { BookCheck } from 'lucide-svelte';
+    import Logo from '$lib/assets/logo.png'
 </script>
 <nav>
-    <BookCheck/>
-    <h1>Pomato</h1>
-    <h1>{ x ? "Login" : "Logout"}</h1>
+    <div>
+        <img src={Logo} alt="logo" class="h-16"/> 
+        <h1>Pomato</h1>
+    </div>
+    <div class="hoverable">
+        <h1>{ x ? "Login" : "Logout"}</h1>
+    </div>
 </nav>
 
 <style>
     nav{
-        @apply bg-primary-50;
+        @apply bg-surface-700 sticky top-0 flex justify-between rounded-lg m-2 py-1;
+    }
+    div {
+        @apply flex items-center mx-4 my-2 space-x-3;
+    }
+    h1 {
+        @apply font-mono font-medium px-2 text-2xl;
+    }
+    .hoverable {
+        @apply rounded-xl px-2 flex hover:bg-error-500;
     }
 </style>
