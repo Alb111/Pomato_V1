d6894abc7e4eb9770b19eda2626869d6b94eaa25
alb123 <albertfelix2005@gmail.com>
2024-07-05 03:20:40 -0700

    top bar done, working on sidebar need to figure away to pass in images

---
+++ src/lib/components/Navigation/Sidebar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    type route= {
+        name:String,
+        link:String,
+        image:string,
+        selected:Boolean
+    }
+    let routes: route[] = [
+        { name:"home", link:"/", selected:true, image:"../assets/time.png" },
+        { name:"home", link:"/", selected:true, image:"" },
+        { name:"home", link:"/", selected:true, image:"" }
+    ]
+</script>
+<nav>
+    {#each routes as route}
+    <div>
+        <img src={route.image} alt="not working">
+    </div>
+    {/each}
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-	import '../app.postcss';
-	import Topbar from '$lib/components/Navigation/Topbar.svelte';
+  import "../app.postcss";
+  import Topbar from "$lib/components/Navigation/Topbar.svelte";
+  import Sidebar from "$lib/components/Navigation/Sidebar.svelte";
 </script>
-<Topbar/>
-<slot />
+
+<main class="h-screen w-screen flex-col">
+  <Topbar />
+  <section class="w-full">
+    <Sidebar />
+    <slot />
+  </section>
+</main>
