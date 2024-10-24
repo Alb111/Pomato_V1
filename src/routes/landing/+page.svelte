8e2a8175b30db7b2f0bc943458eae4e8894de2b8
Alb123 <albertfelix2005@gmail.com>
2024-10-24 16:44:28 -0700

    made some progress on login

---
+++ src/routes/landing/+page.svelte
@@ -0,0 +1,21 @@
+<main class="w-screen h-screen flex justify-center items-center">
+  <div class="h-3/5 w-4/5 bg-surface-700 rounded-xl flex">
+    <section class="flex-col w-1/2 rounded-xl">
+      <h1>Sign In</h1>
+      <label for="email"> Email: </label>
+      <input type="text" id="email" />
+      <label for="password"> Password: </label>
+      <input type="text" id="email" />
+      <div>
+        <button type="button">Sign In</button>
+      </div>
+    </section>
+    <section class="flex-col w-1/2 bg-primary-600 rounded-r-xl">
+      <h1>Hello, Friend!</h1>
+      <h2>Register with your personal details to use all of site features</h2>
+      <div>
+        <button type="button"> Sign up </button>
+      </div>
+    </section>
+  </div>
+</main>
